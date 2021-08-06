06802a823e528f0bc4fa2ae13e4dc4452c5e2899
Luis Isasi Oyolo <luis16isasi@gmail.com>
2021-08-06 16:32:13 -0500

    style: add style for pagination's links

--- src/controllers/views/home.controller.js
+++ src/controllers/views/home.controller.js
@@ -1,6 +1,8 @@
 import homeView from '../../views/Home.html';
 import characterController from '../templates/character.controller';
 import { getCharacters } from '../../api';
+import nextDouble from '../../icons/source_icons_fast-arrow-right.html';
+import next from '../../icons/source_icons_nav-arrow-right.html';
 
 const divElement = document.createElement('div');
 divElement.innerHTML = homeView;
@@ -16,19 +18,27 @@ const HomeController = async (page = 1) => {
 
   const bgAndHover = 'bg-gray-100 hover:bg-gray-200 transition-colors duration-150 ease-in-out';
   containerPagination.innerHTML = `
-    <a href="#/${_page - 2}" ${_page <= 1 ? 'style="pointer-events: none"' : ''} class="${bgAndHover} border border-gray-400 font-semibold rounded-md mx-1 px-1 text-indigo-700 "> << </a>
-    <a href="#/${_page - 1}" ${_page <= 1 ? 'style="pointer-events: none"' : ''} class="${bgAndHover} border border-gray-400 font-bold rounded-md mx-1 px-1 text-indigo-700"> < </a>
-    <span class="border border-gray-400 rounded-md mx-1 px-2 text-indigo-700">${_page}</span>
-    <a href="#/${_page + 1}" ${_page < data.info.pages ? '' : 'style="pointer-events: none"'} class="${bgAndHover} border border-gray-400 font-bold rounded-md mx-1 px-1 text-indigo-700"> > </a>
-    <a href="#/${_page + 2}" ${_page < data.info.pages ? '' : 'style="pointer-events: none"'} class="${bgAndHover} border border-gray-400 font-bold rounded-md mx-1 px-1 text-indigo-700"> >> </a>
+    <a href="#/${_page - 2}" ${_page <= 1 ? 'style="pointer-events: none"' : ''} class="${bgAndHover} border transform rotate-180 h-7 w-7 border-gray-400 font-semibold rounded-md mx-1 px-1 text-black flex justify-center items-center">
+      <div class="w-10"> ${nextDouble}</div>
+    </a>
+    <a href="#/${_page - 1}" ${_page <= 1 ? 'style="pointer-events: none"' : ''} class="${bgAndHover} transform rotate-180 h-7 w-7 border border-gray-400 font-bold rounded-md mx-1 px-1 text-black flex justify-center items-center">
+      ${next}
+    </a>
+    <div class="h-7 w-11 text-center border border-gray-400 rounded-md mx-1 px-2">
+      <span class="font-bold" >${_page}</span>
+    </div>
+    <a href="#/${_page + 1}" ${_page < data.info.pages ? '' : 'style="pointer-events: none"'} class="${bgAndHover} h-7 w-7 border border-gray-400 font-bold rounded-md mx-1 px-1 text-black flex justify-center items-center">
+      ${next}
+    </a>
+    <a href="#/${_page + 2}" ${_page < data.info.pages ? '' : 'style="pointer-events: none"'} class="${bgAndHover} h-7 w-7 border border-gray-400 font-bold rounded-md mx-1 px-1 text-black flex justify-center items-center">
+      <div class="w-10"> ${nextDouble}</div>
+    </a>
   `;
-
-  console.log({ data });
 
   containerCharacters.innerHTML = '';
   // adding all characters within container
   data.results.forEach(character => {
-    containerCharacters.appendChild(characterController(character));
+    containerCharacters.append(characterController(character));
   });
 
   return divElement;
